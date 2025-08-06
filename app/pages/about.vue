255d23c0e951afc4f16a49dfea09a18d4da32c62
Jarin Anan <jarinananmahin03@gmail.com>
2025-08-06 12:14:13 +0600

    Page linking ,fetching id from url and render using uxt and vue

---
+++ app/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Hello from about page</h2>
+         <NuxtLink to="/index">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/portfolio">Portfolio</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/pages/product/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>This page carries Product Details</h2>
+        <h2>{{id}}</h2>
+
+        <NuxtLink to="/index">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/portfolio">Portfolio</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    const id = useRoute().params.id;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
